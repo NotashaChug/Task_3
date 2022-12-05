15a59e3cf95c21a73178defe2b17649eb77b1b28
NotashaChug <NatAChugunova@stud.kpfu.ru>
2022-12-05 18:38:32 +0300

    срезы+график в 3Д

--- task_3/main.py
+++ task_3/main.py
@@ -1,18 +1,39 @@
 
 from astropy.io import fits
 import matplotlib.pyplot as plt
+import numpy as np
 
 def cutX(x,y,d):  # cрез звезды по оси X
-    Ox = list(range(x-d,x+d))
+    Ox = list(range(x-d,x+d+1))
     Oz = data[y][(x-d):(x+d)]
+    plt.plot(Ox,Oz,'r-')
+    plt.show()
 
 
 def cutY(x,y,d):  # срез звезды по оси Y
-    Oy = list(range(y-d,y+d))
+    Oy = list(range(y-d,y+d+1))
     Oz=[]
-    for i in range(y-d,y+d):
+    for i in range(y-d,y+d+1):
         Oz.append(data[i][x])
-    print(Oz)
+    plt.plot(Oy,Oz,'b-')
+    plt.show()
+
+def grafik_star_3d(x,y,d): #функция для 3Д графика
+
+    fig=plt.figure()  #создали пространство
+    ax=plt.axes(projection='3d')
+
+    #пространство заначений для х и y
+    Ox=list(range(x-d,x+d+1))
+    Oy=list(range(y-d,y+d+1))
+
+    Oz=np.zeros((len(Ox),len(Oy))) #заполняем данный массив значениями по оси Oz.
+    for i in range(len(Oy)):      #как бы смотря на пл-ть xOy сверху
+        for j in range(len(Ox)):  #точка (x-d,y-d) в левом верхнем углу
+            Oz[j][i]=data[Oy[i]][Ox[j]]
+
+    ax.plot_surface(Ox,Oy,Oz)
+    plt.show()
 
 
 
@@ -20,5 +41,9 @@ def cutY(x,y,d):  # срез звезды по оси Y
 image = fits.open('v523cas60s-001.fit') #читаем файл
 data = image[0].data #вытаскиваем список числовых данных
 
+'''
 cutX(1024,1024,20)
 cutY(1024,1024,20)
+'''
+
+grafik_star_3d(1024,1024,2)
